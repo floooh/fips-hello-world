fb0e94bfc738a2ab787f4e572cf53f749ae03178
Andre Weissflog <floooh@gmail.com>
2014-12-18 19:21:39 +0100

    Test imported defines

--- src/hello.c
+++ src/hello.c
@@ -4,6 +4,12 @@
 int main() {
     printf("Hello World!\n");
     print_dep1();
+    
+    // check whether imported defines work
+    const char* str_def = TEST_DEFINE_2;
+    printf("Imported string define: %s\n", str_def);
+    printf("Imported int define: %d\n", TEST_DEFINE_1);
+    
     return 0;
 }
 
